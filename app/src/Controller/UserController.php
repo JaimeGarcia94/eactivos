ddd07d6015ece6fa991729c3e783c81f60db37b7
JaimeGarcia94 <jaimegarciagar94@gmail.com>
2020-11-29 18:45:16 +0100

    create new controllers and make form register

---
+++ app/src/Controller/UserController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+use App\Entity\User;
+use App\Form\RegisterType;
+
+class UserController extends AbstractController
+{
+
+    public function register(Request $request)
+    {
+        $user = new User();
+        $form = $this->createForm(RegisterType::class, $user);
+
+        return $this->render('user/register.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
