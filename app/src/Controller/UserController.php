d5ff3d83ff45f66c9236c02bb3f03f6d29fadc98
JaimeGarcia94 <jaimegarciagar94@gmail.com>
2020-11-29 19:18:41 +0100

    fix rename director form uppercase

--- app/src/Controller/UserController.php
+++ app/src/Controller/UserController.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 use App\Entity\User;
-use App\form\RegisterType;
+use App\Form\RegisterType;
 
 class UserController extends AbstractController
 {
