9131cedb209c8b1a027d54a86114c998a67570ab
JaimeGarcia94 <jaimegarciagar94@gmail.com>
2020-11-30 23:16:41 +0100

    fix styles global web

--- app/src/Controller/AuctionController.php
+++ app/src/Controller/AuctionController.php
@@ -6,8 +6,6 @@ use App\Entity\Auction;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Form\AuctionType;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface as UserInterface;
 
 class AuctionController extends AbstractController
@@ -15,7 +13,6 @@ class AuctionController extends AbstractController
 
     public function index()
     {
-
         $em = $this->getDoctrine()->getManager();
         $auction_repo = $this->getDoctrine()->getRepository(Auction::class);
         $auctions = $auction_repo->findAll();
